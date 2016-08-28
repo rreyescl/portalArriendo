10ebb1c89323b47f10fe6dddb0407a78388ae341
Ricardo Reyes <bassricky@gmail.com>
2016-08-27 23:50:41 -0400

    se agrego modelo habitaciones #5

---
+++ modelos/Habitaciones/Habitaciones.php
@@ -0,0 +1,34 @@
+<?php
+class Habitaciones
+{
+  private $id;
+  private $propiedad_id;
+  private $descripcion;
+
+  function __construct(argument)
+  {
+  }
+  public function getId(){
+    return $this->id;
+  }
+  public function setId($id){
+    $this->id = $id;
+  }
+  public function getPropiedadId(){
+    return $this->propiedad_id;
+  }
+  public function setPropiedadId($propiedad_id){
+    $this->propiedad_id = $propiedad_id;
+  }
+  public function getDescripcion(){
+    return $this->descripcion;
+  }
+  public function setDescripcion($descripcion){
+    $this->descripcion = $descripcion;
+  }
+}
+
+
+
+
+?>
--- modelos/Propiedades/Propiedades.php
+++ modelos/Propiedades/Propiedades.php
@@ -1,7 +1,7 @@
 <?php
 /**
- *
- */
+*
+*/
 class Propiedades
 {
   private $id;
@@ -15,68 +15,48 @@ class Propiedades
   function __construct()
   {
 
+  }
+  public function getId(){
+    return $this->id;
+  }
+  public function setId($id){
+    $this->id = $id;
+  }
+  public function getPropietarioId(){
+    return $this->propietario_id;
+  }
+  public function setPropietarioId($propietario_id){
+    $this->propietario_id = $propietario_id;
+  }
+  public function getDireccion(){
+    return $this->direccion;
+  }
+  public function setDireccion($direccion){
+    $this->direccion = $direccion;
+  }
+  public function getDescripcion(){
+    return $this->descripcion;
+  }
+  public function setDescripcion($descripcion){
+    $this->descripcion = $descripcion;
+  }
+  public function getTarifa(){
+    return $this->tarifa;
+  }
+  public function setTarifa($tarifa){
+    $this->tarifa = $tarifa;
+  }
+  public function getComunaId(){
+    return $this->comuna_id;
+  }
+  public function setComunaId($comuna_id){
+    $this->comuna_id = $comuna_id;
+  }
+  public function getCantidadBanos(){
+    return $this->cantidad_baños;
+  }
+  public function setCantidadBanos($cantidad_baños){
+    $this->cantidad_baños = $cantidad_baños;
+  }
 }
-public function getId(){
-return $this->id;
-}
-public function setId($id){
-$this->id = $id;
-}
-public function getPropietarioId(){
-return $this->propietario_id;
-}
-public function setPropietarioId($propietario_id){
-$this->propietario_id = $propietario_id;
-}
-public function getDireccion(){
-return $this->direccion;
-}
-public function setDireccion($direccion){
-$this->direccion = $direccion;
-}
-public function getDescripcion(){
-return $this->descripcion;
-}
-public function setDescripcion($descripcion){
-$this->descripcion = $descripcion;
-}
-public function getTarifa(){
-return $this->tarifa;
-}
-public function setTarifa($tarifa){
-$this->tarifa = $tarifa;
-}
-public function getComunaId(){
-return $this->comuna_id;
-}
-public function setComunaId($comuna_id){
-$this->comuna_id = $comuna_id;
-}
-public function getCantidadBanos(){
-return $this->cantidad_baños;
-}
-public function setCantidadBanos($cantidad_baños){
-$this->cantidad_baños = $cantidad_baños;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
- ?>
+?>
