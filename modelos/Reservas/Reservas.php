bc0837cb9458472db8512dee77b64af80b519381
Ricardo Reyes <bassricky@gmail.com>
2016-08-28 00:44:49 -0400

    se agregan estilos y esas cosas

---
+++ modelos/Reservas/Reservas.php
@@ -0,0 +1,52 @@
+<?php
+/**
+*
+*/
+class ClassName extends AnotherClass
+{
+  private $id;
+  private $id_arrendatario;
+  private $id_habitacion;
+  private $desde;
+  private $hasta;
+
+  function __construct(argument)
+  {
+
+  }
+  public function getId(){
+    return $this->id;
+  }
+  public function setId($id){
+    $this->id = $id;
+  }
+  public function getIdArrendatario(){
+    return $this->id_arrendatario;
+  }
+  public function setIdArrendatario($id_arrendatario){
+    $this->id_arrendatario = $id_arrendatario;
+  }
+  public function getIdHabitacion(){
+    return $this->id_habitacion;
+  }
+  public function setIdHabitacion($id_habitacion){
+    $this->id_habitacion = $id_habitacion;
+  }
+  public function getDesde(){
+    return $this->desde;
+  }
+  public function setDesde($desde){
+    $this->desde = $desde;
+  }
+
+  public function getHasta(){
+    return $this->hasta;
+  }
+  public function setHasta($hasta){
+    $this->hasta = $hasta;
+  }
+
+}
+
+
+?>
--- vistas/index/index.php
+++ vistas/index/index.php
@@ -1,10 +1,66 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
+<?php include "header.php"; ?>
   <body>
-    asasdsadasd
+
+
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4">
+          <div class="panel panel-primary">
+            <div class="panel-heading">BLACK FRIDAY DEAL</div>
+            <div class="panel-body"><img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+            <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="panel panel-danger">
+            <div class="panel-heading">BLACK FRIDAY DEAL</div>
+            <div class="panel-body"><img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+            <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="panel panel-success">
+            <div class="panel-heading">BLACK FRIDAY DEAL</div>
+            <div class="panel-body"><img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+            <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+          </div>
+        </div>
+      </div>
+    </div><br>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4">
+          <div class="panel panel-primary">
+            <div class="panel-heading">BLACK FRIDAY DEAL</div>
+            <div class="panel-body"><img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+            <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="panel panel-primary">
+            <div class="panel-heading">BLACK FRIDAY DEAL</div>
+            <div class="panel-body"><img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+            <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="panel panel-primary">
+            <div class="panel-heading">BLACK FRIDAY DEAL</div>
+            <div class="panel-body"><img src="http://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+            <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+          </div>
+        </div>
+      </div>
+    </div><br><br>
+
+    <footer class="container-fluid text-center">
+      <p>Online Store Copyright</p>
+      <form class="form-inline">Get deals:
+        <input type="email" class="form-control" size="50" placeholder="Email Address">
+        <button type="button" class="btn btn-danger">Sign Up</button>
+      </form>
+    </footer>
+
   </body>
 </html>
